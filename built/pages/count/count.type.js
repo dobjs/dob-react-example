80f0b8e416e16ea79e8a89ee4973545601429b2f
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2017-08-21 10:20:06 +0800

    rm build

--- built/pages/count/count.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Props {
-}
-exports.Props = Props;
-class State {
-}
-exports.State = State;
-//# sourceMappingURL=count.type.js.map
--- built/pages/home/home.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Props {
-}
-exports.Props = Props;
-class State {
-}
-exports.State = State;
-//# sourceMappingURL=home.type.js.map
