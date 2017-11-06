61c6125bee3b8dc9cbffa54ca55ee11e35c63569
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2017-11-06 10:34:56 +0800

    rm custom Partial

--- stores/index.ts
+++ stores/index.ts
@@ -6,17 +6,12 @@ import ApplicationStore from "./application/store";
 import CountAction from "./count/action";
 import CountStore from "./count/store";
 
-const stores = {
+const store = combineStores({
   ApplicationAction,
   ApplicationStore,
   CountAction,
   CountStore
-}
+})
 
-export default combineStores(stores);
-
-type Partial<T> = {
-  [P in keyof T]?: T[P];
-}
-
-export type IStore = Partial<typeof stores>
+export default store
+export type IStore = typeof store
